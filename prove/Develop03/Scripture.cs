1d8b253bbd424799c42e7e659406844ac470d802
Tymur Pushnoy <tymur.pushnoy@gmail.com>
2024-01-24 23:22:41 +0100

    created structure for w3 programming task

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,28 @@
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+
+    public Scripture(Reference reference, string text)
+    {
+        text.Split(" ").ToList();
+    }
+
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return false;
+    }
+
+}
