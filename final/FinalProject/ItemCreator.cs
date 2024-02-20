316f189925b5269edb83bf1804400f0bb00ff357
Tymur Pushnoy <tymur.pushnoy@gmail.com>
2024-02-20 13:53:43 +0100

    final project v2.1

--- final/FinalProject/ItemCreator.cs
+++ final/FinalProject/ItemCreator.cs
@@ -1,8 +1,13 @@
 public class ItemCreator
 {
+    float costResinPerCM3 = 0.05f;
+    float costPerColor = 1.25f;
+    List<Option> centreFillOptionsList = new List<Option>{new Option("Insect", 4.5f), new Option("Fish", 3.5f), new Option("Rock", 2f), new Option("Flower", 1.75f)};
+    List<Option> textureFillOptionsList = new List<Option>{new Option("Glitter", 0.75f), new Option("Stars", 1.25f), new Option("Foil", 1f)};
+
+
+
     Item item = null;
-
-
 
     public Item CreateItem()
     {
@@ -42,11 +47,11 @@ public class ItemCreator
 
         } while (item == null);
 
-        item.AddOption(new ResinOption("resin", item.CalculateVolume(), 0.05f));
+        item.AddOption(new ResinOption("resin", item.CalculateVolume(), costResinPerCM3));
 
-        AskColor();
-        AskCentreFill();
-        AskTextureFill();
+        AskColor(new List<string>{"red", "orange", "yellow", "green", "blue", "purple"});
+        AskFill(centreFillOptionsList, "Pick something to put in the centre of your item:");
+        AskFill(textureFillOptionsList, "Pick something to mix into the texture of your item:");
         AskBase();
         AskText();
 
@@ -273,19 +278,140 @@ public class ItemCreator
 
 
 
-    private void AskColor()
+    private void AskColor(List<string> colorList)
     {
+        int colorPickCountdown = 0;
 
+        Console.Clear();
+
+        while (true)
+        {
+            Console.Write($"How many colors would you like to use for your item? ${costPerColor} per color.\nSelect 0/1/2/3: ");
+            string colorAmountInput = Console.ReadLine();
+
+            if (colorAmountInput == "0")
+            {
+                break;
+            }
+
+            else if (colorAmountInput == "1")
+            {
+                colorPickCountdown = 1;
+                break;
+            }
+
+            else if (colorAmountInput == "2")
+            {
+                colorPickCountdown = 2;
+                break;
+            }
+
+            else if (colorAmountInput == "3")
+            {
+                colorPickCountdown = 3;
+                break;
+            }
+
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid input. Please pick 0, 1, 2 or 3.");
+            }
+        }
+
+
+
+        int colorNameCounter = 1;
+        Console.Clear();
+
+        while (colorPickCountdown > 0)
+        {
+            Console.WriteLine($"Pick Color {colorNameCounter}:");
+
+            int colorListCounter = 1;
+
+            foreach (string color in colorList)
+            {
+                Console.WriteLine($"   {colorListCounter}. {color}");
+                colorListCounter++;
+            }
+
+
+
+            Console.Write("Select one of the options above: ");
+            string colorChoiceInput = Console.ReadLine();
+
+            if (int.TryParse(colorChoiceInput, out int colorChoiceInputInt))
+            {
+                if(colorChoiceInputInt > 0 && colorChoiceInputInt <= colorList.Count)
+                {
+                    item.AddOption(new ColorOption($"Color{colorNameCounter}", colorList[colorChoiceInputInt - 1], costPerColor));
+                    colorNameCounter++;
+                    colorPickCountdown--;
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid input. Please pick a valid number from the list.");
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid input. Please choose a valid integer.");
+            }
+        }
     }
 
-    private void AskCentreFill()
+    private void AskFill(List<Option> fillList, string message)
     {
-        
-    }
+        Console.Clear();
 
-    private void AskTextureFill()
-    {
-        
+        while (true)
+        {
+            Console.WriteLine(message);
+
+            int fillListCounter = 1;
+
+            foreach (Option fill in fillList)
+            {
+                Console.WriteLine($"   {fillListCounter}. {fill.GetName()} - ${fill.GetCost()}");
+                fillListCounter++;
+            }
+
+            Console.WriteLine($"   {fillListCounter}. None");
+
+
+
+            Console.Write("Select one of the options above: ");
+            string fillChoiceInput = Console.ReadLine();
+
+            if (int.TryParse(fillChoiceInput, out int fillChoiceInputInt))
+            {
+                if (fillChoiceInputInt == fillList.Count + 1)
+                {
+                    Console.Clear();
+                    break;
+                }
+                else if(fillChoiceInputInt > 0 && fillChoiceInputInt <= fillList.Count)
+                {
+                    item.AddOption(fillList[fillChoiceInputInt - 1]);
+                    Console.Clear();
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid input. Please pick a valid number from the list.");
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid input. Please choose a valid integer.");
+            }
+        }
     }
 
     private void AskBase()
